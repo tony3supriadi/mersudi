82bc32586675c1edb9afc95d753d7791af7274a8
tony3supriadi <toni@stebasia.com>
2024-02-26 07:42:55 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             input: [
                 'resources/scss/app.scss',
                 'resources/js/app.js',
-                'resources/js/ajax-form-submitter.js'
             ],
             refresh: true,
         }),
